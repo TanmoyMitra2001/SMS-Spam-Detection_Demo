1298c9d7eacf96b6b71559fb3617a67beb4fa3b5
Tanmoy MItra <72865220+TanmoyMitra2001@users.noreply.github.com>
2025-04-10 12:08:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import pickle
 import string
 import re
 import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize, sent_tokenize
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('punkt_tab')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.stem import PorterStemmer
 
 port_stemmer = PorterStemmer()
